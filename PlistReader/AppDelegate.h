ed6d90010708f7b859ef71681112d3de032d4e00
fy <fengyi@wondershare.cn>
2018-11-30 13:37:55 +0800

    Initial Commit

---
+++ PlistReader/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PlistReader
+//
+//  Created by fengyi on 2018/11/30.
+//  Copyright © 2018 fengyi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PlistReader/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PlistReader
+//
+//  Created by fengyi on 2018/11/30.
+//  Copyright © 2018 fengyi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
